ed6aaaf88527734e1252f68262119bc095d9c435
nullinfidel <bearesx@gmail.com>
2025-05-03 04:39:24 +0800

    new

---
+++ SCRIPTS/02_target_only.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+clear
+
+# 使用专属优化
+sed -i 's,-mcpu=generic,-march=armv8-a,g' include/target.mk
+
+# 交换 LAN/WAN 口
+sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
+# 初始化IP和主机名
+sed -i 's/192.168/172.16/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/NatBox/g' package/base-files/files/bin/config_generate
+
+# remove LRNG for 3328
+rm -f target/linux/generic/hack-6.6/696*
+
+#Vermagic
+latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][4-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/profiles.json
+jq -r '.linux_kernel.vermagic' profiles.json >.vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+
+# 预配置一些插件
+cp -rf ../PATCH/files ./files
+
+find ./ -name *.orig | xargs rm -f
+find ./ -name *.rej | xargs rm -f
+
+#exit 0
